72c5211c7af690b71989073f6c692012310b6558
mwicat <mwicat@gmail.com>
2020-04-29 16:22:52 +0200

    0.2.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -51,7 +51,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "BigRedRecorder";
     const char* const  companyName    = "mwicat";
-    const char* const  versionString  = "0.1.0";
-    const int          versionNumber  = 0x100;
+    const char* const  versionString  = "0.2.0";
+    const int          versionNumber  = 0x200;
 }
 #endif
