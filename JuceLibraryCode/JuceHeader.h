df7bcbdc04aedc1a3ac3cfa1ba1bd139e6ac6140
mwicat <mwicat@gmail.com>
2020-04-29 15:45:00 +0200

    Fix version string

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -51,7 +51,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "BigRedRecorder";
     const char* const  companyName    = "mwicat";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "0.1.0";
+    const int          versionNumber  = 0x100;
 }
 #endif
